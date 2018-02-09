3fff5f771108e20e531ebd5beb8467ca2cc813cd
kels-orien <kelvinorien@gmail.com>
2018-02-09 22:31:27 +0100

    third commit

--- server/index.js
+++ server/index.js
@@ -3,17 +3,36 @@ var express = require("express"),
   port = 80,
   app = require("express")(),
   server = http.createServer(app),
+  bodyParser = require("body-parser"),
   io = require("socket.io")(server),
   liveCart;
 
 console.log("Real time POS running");
 console.log("Server started");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.all("/*", function(req, res, next) {
+  // CORS headers
+  res.header("Access-Control-Allow-Origin", "*"); // restrict it to the required domain
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  // Set custom headers for CORS
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-type,Accept,X-Access-Token,X-Key"
+  );
+  if (req.method == "OPTIONS") {
+    res.status(200).end();
+  } else {
+    next();
+  }
+});
 
 app.get("/", function(req, res) {
   res.send(" Real time POS web app running.");
 });
 
-app.use("/api", require("./api/inventory"));
+app.use("/api/inventory", require("./api/inventory"));
 app.use("/api", require("./api/transactions"));
 
 // Websocket logic for Live Cart
